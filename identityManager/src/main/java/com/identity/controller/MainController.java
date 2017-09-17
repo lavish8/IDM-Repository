328af35006b9978433ab4fdadb68242f1ed9a4bb
Shashikant Maheshwari <lavish8@gmail.com>
2017-09-17 18:24:20 +0530

    added command for build

---
+++ identityManager/src/main/java/com/identity/controller/MainController.java
@@ -0,0 +1,13 @@
+package com.identity.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class MainController {
+
+	@RequestMapping("/")
+	public String login() {
+		return "index";
+	}
+}
---
+++ identityManager/src/main/java/com/identity/manager/IdentityManagerApplication.java
@@ -0,0 +1,12 @@
+package com.identity.manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IdentityManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IdentityManagerApplication.class, args);
+	}
+}
