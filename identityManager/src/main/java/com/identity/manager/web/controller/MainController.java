27b269b17d40cdde5adb2d5a0b994757c6585599
Shashikant Maheshwari <lavish8@gmail.com>
2017-09-25 00:03:25 +0530

    contorller page has changed it's location

--- identityManager/src/main/java/com/identity/manager/web/controller/MainController.java
+++ identityManager/src/main/java/com/identity/manager/web/controller/MainController.java
@@ -1,4 +1,4 @@
-package com.identity.manager.controller;
+package com.identity.manager.web.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
