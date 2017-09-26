a9ea7f2855d9f84bdca4caa2de6618f45b64946c
Shashikant Maheshwari <lavish8@gmail.com>
2017-09-27 00:31:30 +0530

    index page has loaded with HTML5 and bootstrap

--- accessmanager/src/main/java/com/access/manager/web/controller/BaseController.java
+++ accessmanager/src/main/java/com/access/manager/web/controller/BaseController.java
@@ -1,4 +1,4 @@
-package com.access.manager.controller;
+package com.access.manager.web.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
