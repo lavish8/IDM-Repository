0d770556451e9e86e1bf5e741c06a3d345002b49
Shashikant Maheshwari <lavish8@gmail.com>
2017-09-29 12:02:10 +0530

    added i18n messages

--- identityManager/src/test/java/com/identity/manager/IdentityManagerApplicationTests.java
+++ identityManager/src/test/java/com/identity/manager/IdentityManagerApplicationTests.java
@@ -1,16 +1,32 @@
 package com.identity.manager;
 
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import com.identity.manager.web.i18n.I18NService;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class IdentityManagerApplicationTests {
+	
+	@Autowired
+	private I18NService i18NService;
 
 	@Test
 	public void contextLoads() {
+		
+	}
+
+	@Test
+	public void testMessageByLocaleService() throws Exception {
+		String expectedResult = "Bootstrap starter template";
+		String messageId = "index.main.header.h1";
+		String actual = i18NService.getMessage(messageId);
+		Assert.assertEquals("The actual and expected Strings don't match", expectedResult, actual);
 	}
 
 }
