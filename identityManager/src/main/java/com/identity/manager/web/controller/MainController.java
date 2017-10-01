92b64d781926814af4de75287ff131b5a1e5e619
Shashikant Maheshwari <lavish8@gmail.com>
2017-10-02 02:24:23 +0530

    configure and save feedback page

--- identityManager/src/main/java/com/identity/manager/web/controller/MainController.java
+++ identityManager/src/main/java/com/identity/manager/web/controller/MainController.java
@@ -3,23 +3,43 @@ package com.identity.manager.web.controller;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.identity.manager.enums.ModelViewEnum;
+import com.identity.manager.web.domain.ContactPojo;
 
 @Controller
 public class MainController {
-	
+
 	/** The application logger */
 	private static final Logger LOG = LoggerFactory.getLogger(MainController.class);
 
 	@RequestMapping("/")
 	public String login() {
-		LOG.debug("################# index {} ############# ", "page");
+		LOG.debug("################# loading index page #############");
 		return "index";
 	}
 
 	@RequestMapping("/about")
-	public String hello() {
-		return "about";
+	public String about() {
+		return ModelViewEnum.ABOUT_US_VIEW_NAME.getId();
+	}
+
+	@RequestMapping(value = "/contact", method = RequestMethod.GET)
+	public String contact(ModelMap model) {
+		LOG.debug("################# loading contact page #############");
+		ContactPojo contact = new ContactPojo();
+		model.addAttribute(ModelViewEnum.FEEDBACK_MODEL, contact);
+		return ModelViewEnum.CONTACT_US_VIEW_NAME.getId();
+	}
+
+	@RequestMapping(value = "/contact", method = RequestMethod.POST)
+	public String contactPost(@ModelAttribute(ModelViewEnum.FEEDBACK_MODEL) ContactPojo contact) {
+		LOG.debug("################# content of contact page {} #############", contact);
+		return ModelViewEnum.CONTACT_US_VIEW_NAME.getId();
 	}
 
 }
---
+++ identityManager/src/main/java/com/identity/manager/web/domain/ContactPojo.java
@@ -0,0 +1,65 @@
+package com.identity.manager.web.domain;
+
+import java.io.Serializable;
+
+public class ContactPojo implements Serializable {
+
+	private static final long serialVersionUID = 8246009030271518087L;
+
+	public ContactPojo() {
+	}
+
+	private String firstName;
+	private String lastName;
+	private String email;
+	private String feedback;
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getFeedback() {
+		return feedback;
+	}
+
+	public void setFeedback(String feedback) {
+		this.feedback = feedback;
+	}
+
+	@Override
+	public String toString() {
+		StringBuilder builder = new StringBuilder();
+		builder.append("ContactPojo [firstName=");
+		builder.append(firstName);
+		builder.append(", lastName=");
+		builder.append(lastName);
+		builder.append(", email=");
+		builder.append(email);
+		builder.append(", feedback=");
+		builder.append(feedback);
+		builder.append("]");
+		return builder.toString();
+	}
+
+
+}
